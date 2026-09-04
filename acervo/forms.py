288423e7ef6be29f7537b8d1afbdb2c92949f8bf
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-09-04 11:01:37 -0300

    começando do 0

--- acervo/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from .models import Livro
-class LivroForm(forms.ModelForm):
-    class Meta:
-        model = Livro
-        fields = ['titulo', 'autor', 'ano']
--- acervo/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Livro(models.Model):
-    titulo = models.CharField(max_length=200)
-    autor = models.CharField(max_length=100)
-    ano = models.IntegerField()
-    disponivel = models.BooleanField(default=True)
