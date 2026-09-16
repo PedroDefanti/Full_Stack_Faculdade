1402beeecfe9ae7cf502c83c132734273526c04d
Pedro  Defanti <168847376+PedroDefanti@users.noreply.github.com>
2026-09-16 20:52:20 -0300

    feat:adição da lógica de busca

--- aulas/acervo/views.py
+++ aulas/acervo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Livro
+from .models import Livro,Acervo
 
 def lista_livros(request):
     livros = Livro.objects.all() # busca no banco
@@ -17,3 +17,29 @@ def novo_livro(request):
     else:
         form = LivroForm()
     return render(request, 'acervo/form.html', {'form': form})   
+
+
+def Acervo(request):
+    nome=request.GET.get("busca","")
+    tipo=request.GET.get("tipo","")
+    categoria=request.GET.get("categoria","")
+    
+    acervo=Acervo.objects.all()
+    
+    if nome:
+        acervo=acervo.filter(nome__icontains=nome)
+        
+    if tipo:
+        acervo=acervo.filter(tipo_acervo=tipo)
+        
+    if categoria:
+        acervo=acervo.filter(categoria=categoria)
+        
+    
+    context={
+        'produto':acervo,
+        'nome':nome,
+        'tipo':tipo,
+        'categoria':categoria
+    }
+    return render(request,'busca.html',context)
