2e800fc4dc8c7eb5c4bbbf99b7110fe36521cd80
Pedro  Defanti <168847376+PedroDefanti@users.noreply.github.com>
2026-09-16 21:28:31 -0300

    feat:adição de novas urls

--- aulas/acervo/urls.py
+++ aulas/acervo/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(
-        'livros/',
-        views.lista_livros,
-        name='lista'
-    ),
+    path('', views.lista_livros, name='lista'),
+    path('busca/', views.buscar_acervo, name='buscar_acervo'),  
+    path('novo_livro/', views.novo_livro, name='novo_livro'),
 ]
