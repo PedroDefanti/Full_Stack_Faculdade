453038af5ca739ff06a32a2405feae3c5d806d01
Pedro  Defanti <168847376+PedroDefanti@users.noreply.github.com>
2026-09-23 21:19:13 -0300

    feat:adição de validação de ano

--- provas/P1/Biblioteca-Acervo/acervo/forms.py
+++ provas/P1/Biblioteca-Acervo/acervo/forms.py
@@ -1,6 +1,21 @@
+from datetime import date
 from django import forms
 from .models import Livro
+
+
 class LivroForm(forms.ModelForm):
+
     class Meta:
         model = Livro
-        fields = ['titulo', 'autor', 'ano']
+        fields = ["titulo", "autor", "ano"]
+
+    def clean_ano(self):
+        ano_livro = self.cleaned_data.get("ano")
+        ano_atual = date.today().year
+
+        if ano_livro and ano_livro > ano_atual:
+            raise forms.ValidationError(
+                f"O ano de publicação ({ano_livro}) não pode ser maior do que o ano atual"
+            )
+
+        return ano_livro
